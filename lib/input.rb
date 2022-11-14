c225f68ebc64014596fb0e9b4a4c9172cd87654a
Kyle Boureston <kyleboureston@gmail.com>
2022-11-14 15:11:09 -0600

    Add include: Dispay to include.rb

--- lib/input.rb
+++ lib/input.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 # Mixin for all inputs requested from user
-module Inputs
+module Input
+  # Include the 'display' module to this module
+  def self.-(_base)
+    include Display
+  end
+
   def player_name_input(player_num)
     puts display_user_name_prompt(player_num)
     player_name = gets.chomp
