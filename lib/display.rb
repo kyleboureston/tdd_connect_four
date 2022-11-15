e0e6a212bbddb00ff02ed708dcf7c91ecf183c01
Kyle Boureston <kyleboureston@gmail.com>
2022-11-14 19:31:37 -0600

    Add new display messages

--- lib/display.rb
+++ lib/display.rb
@@ -7,6 +7,14 @@ module Display
   end
 
   def display_user_name_warning
-    "Try again. Please enter your name using letters only."
+    'Try again. Please enter your name using letters only.'
+  end
+
+  def display_cage_size_prompt
+    'What size connect-four cage do you want to play with? Enter a number between 5 and 15.'
+  end
+
+  def display_cage_size_warning
+    'Try again. Please enter a number between 5 and 15.'
   end
 end
