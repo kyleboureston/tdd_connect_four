68205aa171f7fd1183325087595c6f0cbade3f62
Kyle Boureston <kyleboureston@gmail.com>
2022-11-16 14:43:27 -0600

    Migrate display related items to display/cage.rb

--- lib/cage.rb
+++ lib/cage.rb
@@ -3,6 +3,7 @@
 # Class to create the cage/board to drop the pieces
 class Cage
   attr_reader :cage, :blank_cell, :min_col, :max_col
+  include Display::Cage
 
   def initialize(cage_size)
     @cage       = Array.new(cage_size) { Array.new(cage_size) { 'empty' } }
@@ -23,68 +24,11 @@ class Cage
   end
 
   def update(player_token, col)
-    counter = 0
-    cage_arr = @cage.clone
-    cage_arr.reverse.map! do |row|
-      break if counter == 1
-
+    @cage.reverse.map! do |row|
       row.map!.with_index do |cell, index|
         row_col = index + 1
-        break if counter == 1
-
-        if row_col == col && cell == 'empty'
-          counter += 1
-          player_token
-        else
-          cell
-        end
+        row_col == col && cell == 'empty' ? player_token : cell
       end
     end
-  end
-
-  private
-
-  def print_col_nums(final_col)
-    print '  '
-    [*1..final_col].each { |col| print "#{col} " }
-  end
-
-  def print_circle(circle, index, final_col)
-    if index.zero?
-      print '│ ' + get_circle(circle) + ' '
-    elsif index == (final_col - 1)
-      print get_circle(circle) + ' │'
-    else
-      print get_circle(circle) + ' '
-    end
-  end
-
-  def get_circle(color = 'empty')
-    {
-      'empty' => "\u25CB",
-      'red' => "\u25CF".red,
-      'blue' => "\u25CF".blue,
-      'green' => "\u25CF".green,
-      'yellow' => "\u25CF".yellow,
-      'purple' => "\u25CF".purple,
-      'pink' => "\u25CF".pink,
-      'white' => "\u25CF".white
-    }[color]
-  end
-
-  def display_top(final_col)
-    '┌' + ('─' * ((final_col * 2) + 1)) + '┐'
-  end
-
-  def display_bottom(final_col)
-    '└─' + ('┬─' * final_col) + '┘'
-  end
-
-  def print_spacer1
-    puts
-  end
-
-  def print_spacer2
-    2.times { puts }
   end
 end
--- lib/display/cage.rb
+++ lib/display/cage.rb
@@ -1,8 +1,50 @@
 # frozen_string_literal: true
 
 module Display
-    # Mixin for all messages displayed to the user
-    module Cage
-
+  # Mixin for all messages displayed to the user
+  module Cage
+    def display_top(final_col)
+      '┌' + ('─' * ((final_col * 2) + 1)) + '┐'
     end
+
+    def display_bottom(final_col)
+      '└─' + ('┬─' * final_col) + '┘'
+    end
+
+    def print_spacer1
+      puts
+    end
+
+    def print_spacer2
+      2.times { puts }
+    end
+
+    def print_col_nums(final_col)
+      print '  '
+      [*1..final_col].each { |col| print "#{col} " }
+    end
+  
+    def print_circle(circle, index, final_col)
+      if index.zero?
+        print '│ ' + color(circle) + ' '
+      elsif index == (final_col - 1)
+        print color(circle) + ' │'
+      else
+        print color(circle) + ' '
+      end
+    end
+
+    def color(color = 'empty')
+      {
+        'empty' => "\u25CB",
+        'red' => "\u25CF".red,
+        'blue' => "\u25CF".blue,
+        'green' => "\u25CF".green,
+        'yellow' => "\u25CF".yellow,
+        'purple' => "\u25CF".purple,
+        'pink' => "\u25CF".pink,
+        'white' => "\u25CF".white
+      }[color]
+    end
+  end
 end
