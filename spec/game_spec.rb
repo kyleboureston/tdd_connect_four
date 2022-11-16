9cbd535dba388d6494173a84c277118e2e477f44
Kyle Boureston <kyleboureston@gmail.com>
2022-11-16 16:42:24 -0600

    Add cage#update rspec test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -35,7 +35,7 @@ describe Game do
     context 'when Cage.new is called' do
       it 'sends message :new to Cage with the correct arguments' do
         # Arrange
-        valid_cage_size = 200
+        valid_cage_size = 5
         allow(game).to receive(:clear_screen)
         allow(game).to receive(:puts)
         allow(game).to receive(:cage_size_input).and_return(valid_cage_size)
@@ -43,6 +43,31 @@ describe Game do
         expect(Cage).to receive(:new).with(valid_cage_size)
         # Act
         game.create_cage
+      end
+    end
+  end
+
+  describe 'cage#update' do
+    cage_size = 6
+    subject(:new_cage) { Cage.new(cage_size) }
+    context 'when color and column are sent to to cage#update twice' do
+      it 'adds that color, to that column of @cage twice' do
+        # Arrange
+        color = 'white'
+        column = 1
+        # Act
+        new_cage.update(color, column)
+        new_cage.update(color, column)
+
+        count = 0
+        new_cage.cage.each do |row|
+          row.each_with_index do |cell_color, index|
+            col_index = index + 1
+            count += 1 if col_index == column && cell_color == color
+          end
+        end
+        # Assert
+        expect(count).to be(2)
       end
     end
   end
