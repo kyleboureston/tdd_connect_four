0fd255ae9a3ec0f4fa70d671a0a5ee7fb8bbc2ea
Kyle Boureston <kyleboureston@gmail.com>
2022-11-17 17:10:08 -0600

    Comment out method to import modules from RSpec.config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,7 @@
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.formatter = :documentation
-  config.include Input, Display
+  # config.include Input, Display
 
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
