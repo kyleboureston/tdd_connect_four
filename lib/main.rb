abcd802cff05ddc64c66b0bbd2364b10e036488d
Kyle Boureston <kyleboureston@gmail.com>
2022-11-17 14:09:53 -0600

    Update require_relative to accomodate new file structure

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 require_relative 'colors'
-require_relative 'display'
+require_relative 'display/messages'
+require_relative 'display/cage'
 require_relative 'input'
 require_relative 'player'
 require_relative 'cage'
