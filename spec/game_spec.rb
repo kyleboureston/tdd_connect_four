0eb169cd31686ab424efeac807f19e702d57b712
Kyle Boureston <kyleboureston@gmail.com>
2022-11-17 14:29:09 -0600

    Move cage#update to cage_spec.rb

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -46,29 +46,4 @@ describe Game do
       end
     end
   end
-
-  describe 'cage#update' do
-    cage_size = 6
-    subject(:new_cage) { Cage.new(cage_size) }
-    context 'when color and column are sent to to cage#update twice' do
-      it 'adds that color, to that column of @cage twice' do
-        # Arrange
-        color = 'white'
-        column = 1
-        # Act
-        new_cage.update(color, column)
-        new_cage.update(color, column)
-
-        count = 0
-        new_cage.cage.each do |row|
-          row.each_with_index do |cell_color, index|
-            col_index = index + 1
-            count += 1 if col_index == column && cell_color == color
-          end
-        end
-        # Assert
-        expect(count).to be(2)
-      end
-    end
-  end
 end
